6caf4ce85aea39c69f81e651a820ddab3af1e6ae
AntonyUsoltsev <usoltsev_anton1@mail.ru>
2024-05-12 23:04:04 +0700

    todo

---
+++ src/main/resources/db/migration/V9__offer_insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.offer (id, price, task_id, user_id) VALUES (1, 200.00, 1, 2);
+INSERT INTO public.offer (id, price, task_id, user_id) VALUES (2, 150.00, 1, 53);
+INSERT INTO public.offer (id, price, task_id, user_id) VALUES (3, 1000.00, 2, 53);
