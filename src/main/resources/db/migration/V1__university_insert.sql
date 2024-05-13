ddbf0b09dca42740859fe167c2d6a9fbd7c299c9
AntonyUsoltsev <usoltsev_anton1@mail.ru>
2024-05-13 23:28:37 +0700

    move todo

--- src/main/resources/db/migration/V1__university_insert.sql
+++ src/main/resources/db/migration/V1__university_insert.sql
@@ -1,3 +1,3 @@
-INSERT INTO public.university (id, image_url, name) VALUES (1, 'https://datalib.ru//uploads/images/logo%20univerov/logo/Logo/msu.png', 'Ã√”');
-INSERT INTO public.university (id, image_url, name) VALUES (2, 'https://habrastorage.org/getpro/moikrug/uploads/company/380/907/380/logo/medium_bc34d2797137fa139dde15dc82ea299b.png', 'Õ√”');
+INSERT INTO public.university (id, image_url, name) VALUES (1, 'https://habrastorage.org/getpro/moikrug/uploads/company/380/907/380/logo/medium_bc34d2797137fa139dde15dc82ea299b.png', 'Õ√”');
+INSERT INTO public.university (id, image_url, name) VALUES (2, 'https://datalib.ru//uploads/images/logo%20univerov/logo/Logo/msu.png', 'Ã√”');
 INSERT INTO public.university (id, image_url, name) VALUES (3, 'https://xn----ftbfmepluu.xn--p1ai/storage/c/2022/08/15/1666087428_614392_79.png', 'Õ√”›”');
