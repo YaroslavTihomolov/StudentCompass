c98f31d4660b04e3856cddb9c88e45f5ae91fa8e
spl1t1kszzz <nik17403@gmail.com>
2024-05-14 14:51:36 +0700

    added subjects inserts

--- src/main/resources/db/migration/V3__subject_insert.sql
+++ src/main/resources/db/migration/V3__subject_insert.sql
@@ -1,3 +1,7 @@
 INSERT INTO public.subject (id, course_id, subject_name_id) VALUES (1, 1, 1);
 INSERT INTO public.subject (id, course_id, subject_name_id) VALUES (2, 1, 2);
 INSERT INTO public.subject (id, course_id, subject_name_id) VALUES (3, 1, 3);
+INSERT INTO public.subject (id, course_id, subject_name_id) VALUES (4, 2, 4);
+INSERT INTO public.subject (id, course_id, subject_name_id) VALUES (5, 2, 5);
+INSERT INTO public.subject (id, course_id, subject_name_id) VALUES (6, 2, 6);
+INSERT INTO public.subject (id, course_id, subject_name_id) VALUES (7, 2, 7);
---
+++ src/main/resources/db/migration/V4__subject_name_insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.subject_name (id, name) VALUES (1, 'ОКР');
+INSERT INTO public.subject_name (id, name) VALUES (2, 'История');
+INSERT INTO public.subject_name (id, name) VALUES (3, 'Информатика');
+INSERT INTO public.subject_name (id, name) VALUES (4, 'ДГМА');
+INSERT INTO public.subject_name (id, name) VALUES (5, 'ОПП');
+INSERT INTO public.subject_name (id, name) VALUES (6, 'C++');
+INSERT INTO public.subject_name (id, name) VALUES (7, 'Java');
